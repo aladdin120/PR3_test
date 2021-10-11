0b7505bce2cb931d28a50c40408e9105574fd9d5
aladdin120 <star1354@mail.ru>
2021-10-11 22:09:32 +0300

    add def create_listening_server to main.py

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ import threading
 
 
 class ChatServer:
-    pass
+    clients_list = []
+
+    last_received_message = ""
+
+    def __init__(self):
+        self.server_socket = None
+        self.create_listening_server()
+
+    def create_listening_server(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        local_ip = '127.0.0.1'
+        local_port = 10319
+        # this will allow you to immediately restart a TCP server
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # this makes the server listen to requests coming from other computers on the network
+        self.server_socket.bind((local_ip, local_port))
+        print("Listening for incoming messages..")
+        self.server_socket.listen(5)
+
+
+
+
+if __name__ == "__main__":
+    ChatServer()
