c9c532b86174972fbf57993c8f7a8c1d3824f256
aladdin120 <star1354@mail.ru>
2021-10-11 22:06:11 +0300

    add test to sockets

--- tests.py
+++ tests.py
@@ -1,5 +1,35 @@
 import unittest
 
+import socket
+
+
+
+class SocketTCPTest(unittest.TestCase):
+
+    def setUp(self):
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serv.bind(("", 2000))
+        self.serv.listen(1)
+
+    def tearDown(self):
+        self.serv.close()
+        self.serv = None
+
+
+class SocketUDPTest(unittest.TestCase):
+
+    def setUp(self):
+        self.serv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.serv.bind(("", 2000))
+
+    def tearDown(self):
+        self.serv.close()
+        self.serv = None
+
+
+
 
 
 
